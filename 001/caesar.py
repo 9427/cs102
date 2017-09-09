12ae45b413b966dadedce8edf7c2c5ee1dbc50eb
9427 <meat9427@gmail.com>
2017-09-09 17:53:19 +0300

    Added encrypt_caesar()

---
+++ 001/caesar.py
@@ -0,0 +1,23 @@
+def encrypt_caesar(plaintext):    #p - plaintext, c-ciphertext
+    ciphertext = ''
+    shift = 3
+    upp = 0
+    for i in range(len(plaintext)):
+        f = ord(plaintext[i])
+        if 64 < f < 91:
+            upp = 1
+        f += shift
+        if f < 65:
+            f += 26
+        if upp == 1 and f > 90:
+            f -= 26
+        if upp == 0 and f < 97:
+            f += 26
+        if f > 122:
+            f -= 26
+        ciphertext += chr(f)
+    return ciphertext
+
+
+p = input()
+print(encrypt_caesar(p))
